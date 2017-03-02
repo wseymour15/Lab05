dfb3966b739735807f638880a50cab7eebf8c4a6
lucas-kern <lkern@iastate.edu>
2017-03-02 17:10:31 -0600

    Got the login and checkLogin working

--- checkLogin.php
+++ checkLogin.php
@@ -5,19 +5,20 @@ function check(){
     $str = file_get_contents('users.txt');
     $array = json_decode($str);
         for($i=0;$i<sizeof($array);$i++){
-        if($array[$i]->user == $_POST['user'] && $array[$i]->pass == $_POST['pass']){
+        if($array[$i]->user === $_POST['username'] && $array[$i]->pass === $_POST['password']){
             return true;
         }
     }
-    retun false;
+    return false;
 }
-//header('Content-type: application/json');
-
-$path = 'phpseclib';
-	set_include_path(get_include_path() . PATH_SEPARATOR . $path);
-	include_once('Crypt/RSA.php');
+header('Content-type: application/json');
+//$path = 'phpseclib';
+//	set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+//	include_once('Crypt/RSA.php');
     if(check()){
         echo json_encode("true");
     }
-    echo json_encode("false");
+    else{
+        echo json_encode("false");
+    }
 ?>
